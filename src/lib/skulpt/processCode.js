51abf1ee16ff625892c5423288daeec9ffb300b2
Salvador Garcia Mendez <salvagarciamendez@gmail.com>
2015-03-13 02:55:49 +0100

    anyadida funcionalidad de interpretacion del codigo en python

---
+++ src/lib/skulpt/processCode.js
@@ -0,0 +1,40 @@
+/**
+ * Created by salva-pc on 13/03/15.
+ */
+
+
+$(document).ready(function ()
+{
+    //miliseconds
+    Sk.execLimit = 3;
+
+
+    function outf(text) {
+        var mypre = document.getElementById("output");
+        mypre.innerHTML = mypre.innerHTML + text;
+    }
+    function builtinRead(x) {
+        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined) throw "File not found: '" + x + "'";
+        return Sk.builtinFiles["files"][x];
+    }
+    $("#runCode").click(function()
+    {
+        document.getElementById("output").innerHTML = "";
+        var editor = ace.edit("editor");
+        code = editor.getSession().getValue();
+
+        Sk.configure({output:outf, read:builtinRead});
+        try
+        {
+            eval(Sk.importMainWithBody("<stdin>",false,code));
+        }
+        catch (e)
+        {
+            console.log(e.toString());
+        }
+
+
+    });
+
+
+});
