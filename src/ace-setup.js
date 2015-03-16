b745f71934c0b4da043528e04b814e442114f6f8
jgm88 <jgm79@outlook.com>
2015-03-16 19:42:19 +0100

    Anyadidos assets de entidades y fases

--- src/ace-setup.js
+++ src/ace-setup.js
@@ -12,26 +12,6 @@ $( document ).ready(function(){
         enableBasicAutocompletion: true
     });
 
-    //var snippetManager = ace.require("ace/snippets").snippetManager;
-    //var config = ace.require("ace/config");
-    //
-    //ace.config.loadModule("ace/snippets/javascript", function(m) {
-    //    if (m) {
-    //        snippetManager.files.javascript = m;
-    //        m.snippetText += mySnippetText; // if you have snippets in the ace snippet format
-    //        m.snippets = snippetManager.parseSnippetFile(m.snippetText);
-    //
-    //        // or do this if you already have them parsed
-    //        m.snippets.push({
-    //            content: "${1:class_name}.prototype.${2:method_name} = function(${3:first_argument}) {    ${4:// body...}",
-    //            name: "proto",
-    //                    tabTrigger: "proto"
-    //        });
-    //
-    //        snippetManager.register(m.snippets, m.scope);
-    //    }
-    //});
-
     //LIMITA LAS LINEAS A 5
 
     $( "#editor" ).keyup(function() {
@@ -53,9 +33,32 @@ $( document ).ready(function(){
 
     $("#load-btn").on('click', function () {
         if(localStorage.getItem('key')){
-                editor.setValue(JSON.parse(localStorage.getItem('key')), -1);
+            editor.setValue(JSON.parse(localStorage.getItem('key')), -1);
             localStorage.removeItem('key');
         }
 
     });
+
+
+    //var snippetManager = ace.require("ace/snippets").snippetManager;
+    //var config = ace.require("ace/config");
+    //
+    //ace.config.loadModule("ace/snippets/javascript", function(m) {
+    //    if (m) {
+    //        snippetManager.files.javascript = m;
+    //        m.snippetText += mySnippetText; // if you have snippets in the ace snippet format
+    //        m.snippets = snippetManager.parseSnippetFile(m.snippetText);
+    //
+    //        // or do this if you already have them parsed
+    //        m.snippets.push({
+    //            content: "${1:class_name}.prototype.${2:method_name} = function(${3:first_argument}) {    ${4:// body...}",
+    //            name: "proto",
+    //                    tabTrigger: "proto"
+    //        });
+    //
+    //        snippetManager.register(m.snippets, m.scope);
+    //    }
+    //});
+
+
 });
