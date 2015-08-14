2db4734509209d3589220821da9ebcc0154756c6
jgm88 <jgm79@outlook.com>
2015-08-14 19:46:31 +0200

    Creadas clases herencia de CDV

---
+++ src/game/cdvs/DronCdv.js
@@ -0,0 +1,23 @@
+/**
+ * Created by javi on 14/08/15.
+ */
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+///<reference path="../../../build/phaser.d.ts"/>
+///<reference path="BaseCdv.ts"/>
+var Roboycod;
+(function (Roboycod) {
+    var DronCdv = (function (_super) {
+        __extends(DronCdv, _super);
+        function DronCdv() {
+            _super.apply(this, arguments);
+        }
+        return DronCdv;
+    })(Roboycod.BaseCdv);
+    Roboycod.DronCdv = DronCdv;
+})(Roboycod || (Roboycod = {}));
+//# sourceMappingURL=DronCdv.js.map
---
+++ src/game/cdvs/WeaponCdv.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by javi on 14/08/15.
+ */
+
+///<reference path="../../../build/phaser.d.ts"/>
+///<reference path="BaseCdv.ts"/>
+
+module Roboycod {
+
+    export class WeaponCdv extends BaseCdv {
+    }
+}
