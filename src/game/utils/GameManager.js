bbbe005e0c17bf5c83c25519a39917472d869eaf
jgm88 <jgm79@outlook.com>
2015-08-18 21:56:23 +0200

    Enriquecida la clase GameManager guarda, carga y devuelve el estado de la partida

--- src/game/utils/GameManager.js
+++ src/game/utils/GameManager.js
@@ -2,6 +2,7 @@
  * Created by javi on 18/08/15.
  */
 ///<reference path="../../../build/phaser.d.ts"/>
+///<reference path="../states/Game.ts"/>
 var Roboycod;
 (function (Roboycod) {
     var GameManager = (function () {
@@ -17,9 +18,34 @@ var Roboycod;
             }
             return GameManager._instance;
         };
-        GameManager.prototype.save = function () {
+        GameManager.save = function (game) {
+            if (localStorage.getItem(GameManager.key)) {
+                localStorage.removeItem(GameManager.key);
+            }
+            GameManager.data = game.cache.getJSON('gameData');
+            localStorage.setItem(GameManager.key, GameManager.data);
+        };
+        /**
+         * Trata de cargar los datos de la memoria y si no carga los iniciales
+         * @param game
+         */
+        GameManager.load = function (game) {
+            if (localStorage.getItem(GameManager.key)) {
+                GameManager.data = localStorage.getItem(GameManager.key);
+            }
+            GameManager.data = game.cache.getJSON('gameData');
+        };
+        GameManager.getData = function (game) {
+            if (GameManager.data === undefined) {
+                GameManager.load(game);
+            }
+            return GameManager.data;
+        };
+        GameManager.clearData = function () {
+            GameManager.data = undefined;
         };
         GameManager._instance = null;
+        GameManager.key = 'roboycodData';
         return GameManager;
     })();
     Roboycod.GameManager = GameManager;
--- src/game/utils/GameManager.ts
+++ src/game/utils/GameManager.ts
@@ -2,12 +2,17 @@
  * Created by javi on 18/08/15.
  */
 ///<reference path="../../../build/phaser.d.ts"/>
-    
+///<reference path="../states/Game.ts"/>
+
 module Roboycod{
 
     export class GameManager{
 
         private static _instance:GameManager = null;
+
+        private static key  : string = 'roboycodData';
+
+        private static data : any;
 
         constructor() {
             if(GameManager._instance){
@@ -22,8 +27,32 @@ module Roboycod{
             }
             return GameManager._instance;
         }
-        public save() :void{
+        public static save(game : Game) :void{
+            if(localStorage.getItem(GameManager.key)){
+                localStorage.removeItem(GameManager.key);
+            }
+            GameManager.data = game.cache.getJSON('gameData');
+            localStorage.setItem(GameManager.key, GameManager.data);
+        }
 
+        /**
+         * Trata de cargar los datos de la memoria y si no carga los iniciales
+         * @param game
+         */
+        public static load(game : Game){
+            if(localStorage.getItem(GameManager.key)){
+                GameManager.data = localStorage.getItem(GameManager.key);
+            }
+            GameManager.data = game.cache.getJSON('gameData');
+        }
+        public static getData(game : Game) : any{
+            if(GameManager.data === undefined){
+                GameManager.load(game);
+            }
+            return GameManager.data;
+        }
+        public static clearData(){
+            GameManager.data = undefined;
         }
     }
 }
